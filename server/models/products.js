754b7cf2ce0cef31400d26ea1ce33f71c14b7872
Warren Wong <warren.wong4@gmail.com>
2021-06-23 14:21:23 -0400

    Establish products model

---
+++ server/models/products.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../../db/index.js');
+
+const Products = sequelize.define('Products', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    unique: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  slogan: {
+    type: DataTypes.STRING(180),
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING(450),
+    allowNull: false,
+  },
+  category: {
+    type: DataTypes.STRING(80),
+    allowNull: false,
+  },
+  default_price: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+Products.sync({ alter: true });
+
+module.exports.Products = Products;
