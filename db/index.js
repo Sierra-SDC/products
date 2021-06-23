2f775cbccd48225578ec68f1be7461833b76a1f6
Warren Wong <warren.wong4@gmail.com>
2021-06-23 11:55:06 -0400

    Establish sequelize connection

--- db/index.js
+++ db/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { Pool } = require('pg');
+const { Sequelize } = require('sequelize');
 
 const pool = new Pool({
   user: process.env.PGUSER,
@@ -13,3 +14,20 @@ pool.query('SELECT NOW()', (err, res) => {
   console.log(err, res);
   pool.end();
 });
+
+const sequelize = new Sequelize(
+  process.env.PGDATABASE,
+  process.env.PGUSER,
+  process.env.PGPASSWORD,
+  {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: 'postgres',
+    logging: (...msg) => console.log(msg),
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => console.log('Connection has been established successfully.'))
+  .catch((error) => console.error('Unable to connect to the database:', error));
