ce1e2a7bd28143f1ebe5c122f47b71b5965fe91c
Warren Wong <warren.wong4@gmail.com>
2021-06-26 16:30:28 -0400

    Update ETL for dynamic seeding
    Included fs.readdir in order to dynamically seed all csv files.

--- db/ETL.js
+++ db/ETL.js
@@ -10,37 +10,43 @@ const {
   Styles,
 } = require('../server/models/index.js');
 
-// Swap csv file and respective Model
-const input = fs.createReadStream(__dirname + '/data/skus.csv', {
-  highWaterMark: 48,
-});
+fs.readdir(__dirname + '/data', (err, files) => {
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      const input = fs.createReadStream(__dirname + '/data/skus.csv', {
+        highWaterMark: 48,
+      });
 
-const parser = csv.parse({
-  columns: true,
-  relax: true,
-});
+      const parser = csv.parse({
+        columns: true,
+        relax: true,
+      });
 
-const inserter = async.cargo((tasks, inserterCallback) => {
-  Skus.bulkCreate(tasks, {
-    ignoreDuplicates: true,
-    benchmark: true,
-  }).then(() => inserterCallback());
-}, 1000);
+      const inserter = async.cargo((tasks, inserterCallback) => {
+        Skus.bulkCreate(tasks, {
+          ignoreDuplicates: true,
+          benchmark: true,
+        }).then(() => inserterCallback());
+      }, 1000);
 
-parser.on('readable', function () {
-  while ((line = parser.read())) {
-    inserter.push(line);
+      parser.on('readable', function () {
+        while ((line = parser.read())) {
+          inserter.push(line);
+        }
+      });
+
+      parser.on('error', function (err) {
+        console.error(err.message);
+      });
+
+      parser.on('end', function (count) {
+        inserter.drain = function () {
+          doneLoadingCallback();
+        };
+      });
+
+      input.pipe(parser);
+    });
   }
 });
-
-parser.on('error', function (err) {
-  console.error(err.message);
-});
-
-parser.on('end', function (count) {
-  inserter.drain = function () {
-    doneLoadingCallback();
-  };
-});
-
-input.pipe(parser);
